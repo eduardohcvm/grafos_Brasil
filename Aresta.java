47e1c1878213a3f6142bf2221204021ca3e3a522
Douglas Andrade <31628610+andradedouglas@users.noreply.github.com>
2024-04-12 08:21:39 -0300

    Add files via upload

---
+++ Aresta.java
@@ -0,0 +1,18 @@
+package app;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Aresta {
+    private String nome;
+    private Vertice origem;
+    private Vertice destino;
+
+    public String toString() {
+        return "Aresta " + (nome.isEmpty() ? ":" : nome + ": ") + "("+ origem.getNome()+ ", " + destino.getNome() + ")";
+    }
+}
---
+++ Vertice.java
@@ -0,0 +1,24 @@
+package app;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class Vertice {
+    private String nome;
+    private List<Vertice> adjacencias;
+    private int grau = 0;
+
+    public Vertice(String nome) {
+        this.nome = nome;
+        adjacencias = new ArrayList<>();
+    }
+    public String toString() {
+        return "Vertice " + nome + " com grau " + grau;
+    }
+}
